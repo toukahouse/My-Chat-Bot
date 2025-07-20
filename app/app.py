f8a93def99c26243761d195b09ead6a482caf668
aa8786122 <aa8786122@gmail.com>
2025-07-20 10:56:20 +0800

    feat: update ringkas manual

--- app/app.py
+++ app/app.py
@@ -149,19 +149,19 @@ def stream_generator(
             safety_settings=[  # <-- Langsung didefinisikan di sini
                 types.SafetySetting(
                     category=types.HarmCategory.HARM_CATEGORY_HARASSMENT,
-                    threshold=types.HarmBlockThreshold.HARM_BLOCK_THRESHOLD_UNSPECIFIED,
+                    threshold=types.HarmBlockThreshold.BLOCK_NONE,
                 ),
                 types.SafetySetting(
                     category=types.HarmCategory.HARM_CATEGORY_HATE_SPEECH,
-                    threshold=types.HarmBlockThreshold.HARM_BLOCK_THRESHOLD_UNSPECIFIED,
+                    threshold=types.HarmBlockThreshold.BLOCK_NONE,
                 ),
                 types.SafetySetting(
                     category=types.HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT,
-                    threshold=types.HarmBlockThreshold.HARM_BLOCK_THRESHOLD_UNSPECIFIED,
+                    threshold=types.HarmBlockThreshold.BLOCK_NONE,
                 ),
                 types.SafetySetting(
                     category=types.HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT,
-                    threshold=types.HarmBlockThreshold.HARM_BLOCK_THRESHOLD_UNSPECIFIED,
+                    threshold=types.HarmBlockThreshold.BLOCK_NONE,
                 ),
             ],
         )
